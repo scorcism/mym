854e3cae82d94e5b5fac6db003bfa940eeb155c3
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-06-21 01:25:12 +0530

    Notes app done

--- Project/index.js
+++ Project/index.js
@@ -1,17 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const app = express()
+const Note = require("./models/Note")
+const User = require("./models/User")
 const port = 3000
 app.use(express.json({ extended: true }))
 app.use(express.urlencoded())
-
-mongoose.connect('mongodb+srv://scor32k:scor32k@cluster0.cw5duyv.mongodb.net/notes').then(()=>{
-    console.log("connected");  
+const JWT_SECRET = "scorcimisscorcism"
+mongoose.connect('mongodb+srv://scor32k:scor32k@cluster0.cw5duyv.mongodb.net/notes').then(() => {
+    console.log("connected");
     // scorcism
 })
-.catch((error)=>{
-    console.log("error: ", error )
-})
+    .catch((error) => {
+        console.log("error: ", error)
+    })
 
 app.get('/', (req, res) => {
     res.sendFile('pages/index.html', { root: __dirname })
@@ -26,27 +30,113 @@ app.get('/signup', (req, res) => {
 })
 
 // APIs endpoint
-app.get('/getnotes', (req, res) => {
+app.get('/getnotes', async (req, res) => {
+    let token = req.header("auth-token")
 
-    res.sendFile('pages/signup.html', { root: __dirname })
+    if (!token) {
+        return res.status(401).json({ success: false, message: "2Authenticate using valid token" })
+    }
+    try {
+        const data = jwt.verify(token, JWT_SECRET);
+        let useremail = data.user.email;
+        console.log(useremail)
+        try {
+            console.log("in try try")
+            let notes = await Note.find({ email: useremail })
+            res.status(200).json({success: true, message:notes})
+        } catch (error) {
+            return res.status(401).json({ success: false, message: "1Authenticate using valid token" })
+        }
+
+    } catch (error) {
+        return res.status(401).json({ success: false, message: "Internal server error" })
+    }
+
+
 })
 
-app.get('/login', (req, res) => {
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email })
+        // // console.log(user.email, user.password)
 
-    res.sendFile('pages/signup.html', { root: __dirname })
+        if (!user) {
+            return res.status(400).json({ success: false, message: "Check Credentials" })
+        }
+
+        const passwordCompare = await bcrypt.compare(password, user.password);
+
+        if (!passwordCompare) {
+            return res.status(400).json({ success: false, message: "Check Credentials" });
+        }
+
+        let data = {
+            user: {
+                email: user.email
+            }
+        }
+
+        let authtoken = jwt.sign(data, JWT_SECRET);
+
+        res.status(200).json({ success: true, message: authtoken })
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ success: false, message: "Check Credentials" });
+    }
 })
 
-app.get('/signup', (req, res) => {
+app.post('/signup', async (req, res) => {
+    // console.log(req.body)
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email })
+        if (user) {
+            return res.status(400).json({ success: false, message: "User already exist" })
+        }
 
-    res.sendFile('pages/signup.html', { root: __dirname })
+        const salt = await bcrypt.genSaltSync(10);
+        const secPassword = await bcrypt.hash(password, salt);
+
+        user = await User.create({ email: email, password: secPassword });
+
+        res.status(200).json({ success: true, message: user })
+
+    } catch (error) {
+        return res.status(400).json({ success: false, message: "Some Error occured" })
+    }
+
 })
 
-app.get('/deletenoe', (req, res) => {
+app.post('/addnote', async (req, res) => {
+    let token = req.header("auth-token")
+    // console.log(token)
+    if (!token) {
+        return res.status(401).json({ success: false, message: "Authenticate using valid token" })
+    }
+    try {
+        const data = jwt.verify(token, JWT_SECRET);
+        let useremail = data.user.email;
 
-    res.sendFile('pages/signup.html', { root: __dirname })
+        try {
+            // console.log(req.body);
+            // console.log(useremail);
+            let newNote = await Note.create({ title: req.body.title, desc: req.body.title, type: req.body.type, email: useremail })
+
+            res.status(200).json({ success: true, message: "Note added" })
+
+        } catch (error) {
+            return res.status(401).json({ success: false, message: "Authenticate using valid token" })
+        }
+
+    } catch (error) {
+        return res.status(401).json({ success: false, message: "Authenticate using valid token" })
+    }
+
 })
 
-app.get('/addnote', (req, res) => {
+app.get('/deletenote', (req, res) => {
 
     res.sendFile('pages/signup.html', { root: __dirname })
 })
--- Project/models/Note.js
+++ Project/models/Note.js
@@ -1,1 +1,22 @@
+const mongoose = require("mongoose");
 
+const noteSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        requred: true
+    },
+    title: {
+        type: String,
+        requred: true
+    },
+    desc: {
+        type: String,
+        requred: true
+    },
+    type: {
+        type: String,
+        default:"personal"
+    },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Note', noteSchema);
